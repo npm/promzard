45b1aac9f7f823c49b454fdc576748e79bfcffe8
isaacs <i@izs.me>
2012-06-13 15:10:17 -0700

    An example that is like npm init

---
+++ example/npm-init/init.js
@@ -0,0 +1,34 @@
+var PZ = require('../../promzard').PromZard
+var path = require('path')
+var input = path.resolve(__dirname, 'init.json')
+
+var fs = require('fs')
+var package = path.resolve(__dirname, 'package.json')
+var pkg
+
+fs.readFile(package, 'utf8', function (er, d) {
+  if (er) ctx = {}
+  try { ctx = JSON.parse(d); pkg = JSON.parse(d) }
+  catch (e) { ctx = {} }
+
+  ctx.dirname = path.dirname(package)
+  ctx.basename = path.basename(ctx.dirname)
+  if (!ctx.version) ctx.version = undefined
+
+  // this should be replaced with the npm conf object
+  ctx.config = {}
+
+  var pz = new PZ(input, ctx)
+
+  pz.on('data', function (data) {
+    console.error('pz data', data)
+    if (!pkg) pkg = {}
+    Object.keys(data).forEach(function (k) {
+      if (data[k] !== undefined && data[k] !== null) pkg[k] = data[k]
+    })
+    fs.writeFile(package, JSON.stringify(pkg, null, 2), function (er) {
+      if (er) throw er
+      console.log('ok')
+    })
+  })
+})
